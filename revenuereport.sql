abc63bfd6962953863ed08d2bcc27632296ea9d3
roedzn <rohann_dizon@dlsu.edu.ph>
2024-11-22 21:53:45 +0800

    Update revenuereport.sql

--- revenuereport.sql
+++ revenuereport.sql
@@ -1,11 +1,11 @@
 select sum(t.AmountPaid) as Payments
-from service s join  transaction t ON t.TransactionID = s.TransactionID
-join appointment ap ON s.serviceID = ap.ServiceID;
+from services s join  transactions t ON t.TransactionID = s.TransactionID
+join appointments ap ON s.serviceID = ap.ServiceID;
 
 
 select sum(t.AmountPaid) as totalBookedPayments
-from service s join  transaction t ON t.TransactionID = s.TransactionID
-join appointment ap ON s.serviceID = ap.ServiceID
+from services s join  transactions t ON t.TransactionID = s.TransactionID
+join appointments ap ON s.serviceID = ap.ServiceID
 where ap.status = "booked";
 
 select sum(tr.TherapistRevenue) as totalTherapistRevenue
