94d79b2be368b1ba78de2ab3748ac61cd628b671
QAZYD <132415864+QAZYD@users.noreply.github.com>
2024-11-22 22:38:14 +0800

    Update payingservice.sql

--- payingservice.sql
+++ payingservice.sql
@@ -1,6 +1,12 @@
+--Variables to be set before running the query:
+--@payingClientID: ID of the client payinf the transaction.
+--@receivingClientID: ID of client recieving the service.
+--@transactionDate: date of transaction.
+--@Amount_Paid: the amount paid by the paying client whcih should be equal to the service cost and therapist rate.
+
 INSERT INTO transactions (payingClientID, receivingClientID, transactionDate, AmountPaid)
-SELECT 10, 10, NOW(), 500.00
+SELECT @payingClientID,@receivingClientID, @transactionDate, @Amount_Paid
 FROM serviceTypes si
 JOIN services s ON si.ServiceTypeID = s.ServiceTypeID
 JOIN therapists th ON s.TherapistID = th.TherapistID
-WHERE s.sessionCost + th.SessionRate = 500.00;
+WHERE s.sessionCost + th.SessionRate = @Amount_Paid;
