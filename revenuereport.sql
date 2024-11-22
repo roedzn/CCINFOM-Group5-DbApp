04b19e1b5884c66e3aa499b8711c853a55507fb8
QAZYD <132415864+QAZYD@users.noreply.github.com>
2024-11-22 20:07:01 +0800

    Update revenuereport.sql

--- revenuereport.sql
+++ revenuereport.sql
@@ -1,12 +1,12 @@
-select sum(t.AmountPaid) as totalLifetimeRevenue
+select sum(t.AmountPaid) as Payments
 from service s join  transaction t ON t.TransactionID = s.TransactionID
 join appointment ap ON s.serviceID = ap.ServiceID;
 
 
-select sum(t.AmountPaid) as totalLifetimeRevenue
+select sum(t.AmountPaid) as totalBookedPayments
 from service s join  transaction t ON t.TransactionID = s.TransactionID
 join appointment ap ON s.serviceID = ap.ServiceID
 where ap.status = "booked";
 
-
-
+select sum(tr.TherapistRevenue) as totalTherapistRevenue
+from therapist_revenue tr
