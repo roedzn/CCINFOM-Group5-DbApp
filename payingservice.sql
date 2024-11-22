368a93b2a90d1ea8af67a70c141c9ac95a8f8aaa
roedzn <rohann_dizon@dlsu.edu.ph>
2024-11-22 21:53:05 +0800

    Update payingservice.sql

--- payingservice.sql
+++ payingservice.sql
@@ -1,6 +1,6 @@
-INSERT INTO transaction (payingClientID, receivingClientID, transactionDate, AmountPaid)
+INSERT INTO transactions (payingClientID, receivingClientID, transactionDate, AmountPaid)
 SELECT 10, 10, NOW(), 500.00
-FROM serviceType si
-JOIN service s ON si.ServiceTypeID = s.ServiceTypeID
-JOIN therapist th ON s.TherapistID = th.TherapistID
+FROM serviceTypes si
+JOIN services s ON si.ServiceTypeID = s.ServiceTypeID
+JOIN therapists th ON s.TherapistID = th.TherapistID
 WHERE s.sessionCost + th.SessionRate = 500.00;
